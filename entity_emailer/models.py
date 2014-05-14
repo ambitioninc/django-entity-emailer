6b1f4bd00dadb9da31b5a86d32b34aa4dd1b16ae
Erik Swanson <theerikswanson@gmail.com>
2014-05-14 14:57:10 -0400

    EmailType model.

--- entity_emailer/models.py
+++ entity_emailer/models.py
@@ -1,7 +1,7 @@
-
 from django.db import models
 from entity.models import Entity
 from jsonfield import JSONField
+
 
 class Email(models.Model):
     email_type = models.ForeignKey(EmailType)
@@ -12,3 +12,8 @@ class Email(models.Model):
     uid = models.CharField(max_length=100, unique=True, null=True)
     scheduled = models.DateTimeField(null=True)
     sent = models.DateTimeField(null=True)
+
+
+class EmailType(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
