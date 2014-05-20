ccfc056bf80f515f643c6fd954bc68de6d11d5a2
Erik Swanson <theerikswanson@gmail.com>
2014-05-20 11:17:38 -0400

    Entity filtering test and style cleanup.

--- entity_emailer/tests/test_tasks.py
+++ entity_emailer/tests/test_tasks.py
@@ -4,34 +4,31 @@ from django.test import TestCase
 from django_dynamic_fixture import G, N
 
 from entity_emailer import tasks
-from entity_emailer.models import Email
+from entity_emailer.models import Email, Unsubscribed
 
 
 class Test_get_email_addresses(TestCase):
     def setUp(self):
         self.ct = ContentType.objects.get_for_model(Email)
-        self.super_entity = G(
-            Entity,
-            entity_meta={'email': 'test_super@example.com'},
-            entity_type=self.ct
-        )
-        self.sub_entity_1 = G(
-            Entity,
-            entity_meta={'email': 'test_sub1@example.com'},
-            entity_type=self.ct
-        )
-        self.sub_entity_2 = G(
-            Entity,
-            entity_meta={'email': 'test_sub2@example.com'},
-            entity_type=self.ct
-        )
+        self.ct2 = ContentType.objects.get_for_model(Unsubscribed)
+        self.super_entity = G(Entity, entity_meta={'email': 'test_super@example.com'}, entity_type=self.ct)
+        self.sub_entity_1 = G(Entity, entity_meta={'email': 'test_sub1@example.com'}, entity_type=self.ct)
+        self.sub_entity_2 = G(Entity, entity_meta={'email': 'test_sub2@example.com'}, entity_type=self.ct)
+        self.sub_entity_3 = G(Entity, entity_meta={'email': 'test_sub3@example.com'}, entity_type=self.ct2)
         G(EntityRelationship, sub_entity=self.sub_entity_1, super_entity=self.super_entity)
         G(EntityRelationship, sub_entity=self.sub_entity_2, super_entity=self.super_entity)
+        G(EntityRelationship, sub_entity=self.sub_entity_3, super_entity=self.super_entity)
 
     def test_returns_sub_entities_emails(self):
         email = N(Email, send_to=self.super_entity, subentity_type=self.ct, context={})
         addresses = tasks.get_email_addresses(email)
         expected_addresses = {u'test_sub1@example.com', u'test_sub2@example.com'}
+        self.assertEqual(set(addresses), expected_addresses)
+
+    def test_filters_other_entity_types(self):
+        email = N(Email, send_to=self.super_entity, subentity_type=self.ct2, context={})
+        addresses = tasks.get_email_addresses(email)
+        expected_addresses = {u'test_sub3@example.com'}
         self.assertEqual(set(addresses), expected_addresses)
 
     def test_returns_own_email(self):
