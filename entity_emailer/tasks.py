2e5072cc226096f904502141801db3aada32acaf
Erik Swanson <theerikswanson@gmail.com>
2014-05-20 21:19:40 -0400

    flake8

--- entity_emailer/tasks.py
+++ entity_emailer/tasks.py
@@ -51,6 +51,7 @@ def get_html_message(email):
         message_template = Template(message_template_file.read())
     return message_template.render(Context(email.context))
 
+
 def get_text_message(email):
     """Load and render the template.
 
