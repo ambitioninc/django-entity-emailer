f351f26780a4900dfa9a5bc3da98e7d0cf2344f0
Erik Swanson <theerikswanson@gmail.com>
2014-05-22 14:03:13 -0400

    get_email_addresses optimization.

--- entity_emailer/tasks.py
+++ entity_emailer/tasks.py
@@ -33,7 +33,8 @@ def get_email_addresses(email):
         all_entities = email.send_to.get_sub_entities().is_type(email.subentity_type)
     else:
         all_entities = [email.send_to]
-    dont_send_to = Unsubscribed.objects.filter(unsubscribed_from=email.email_type)
+    dont_send_to = Unsubscribed.objects.filter(
+        unsubscribed_from=email.email_type).select_related('entity')
     send_to = set(all_entities) - set(unsub.entity for unsub in dont_send_to)
     emails = [e.entity_meta['email'] for e in send_to]
     return emails
