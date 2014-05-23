ab7e22234efb896dbac6d386f5f58e2044839f6f
Erik Swanson <theerikswanson@gmail.com>
2014-05-23 17:34:01 -0400

    Simplify tests for new template system.

--- entity_emailer/tests/test_handlers.py
+++ entity_emailer/tests/test_handlers.py
@@ -63,29 +63,25 @@ class HandelEmailSaveTest(TestCase):
         self.assertEqual(len(mail.outbox), 1)
 
     @override_settings(CELERY_ALWAYS_EAGER=True, CELERY_EAGER_PROPAGATES_EXCEPTIONS=True, BROKER_BACKEND='memory')
-    @patch('entity_emailer.tasks.render_to_string')
     @patch('entity_emailer.tasks.get_email_addresses')
-    def test_from_field_pulled_from_default_settings(self, address_mock, loader_mock):
+    def test_from_field_pulled_from_default_settings(self, address_mock):
         """settings.DEFAULT_FROM_EMAIL should be passed through.
         """
-        loader_mock.side_effect = ['<p>This is a test html email.</p>',
-                                   'This is a test text email.']
+        template = G(EmailTemplate, text_template='Hi')
         address_mock.return_value = ['test1@example.com', 'test2@example.com']
-        G(Email, context={}, scheduled=None)
+        G(Email, context={}, scheduled=None, template=template)
         from_email = mail.outbox[0].from_email
         self.assertEqual(from_email, 'test@example.com')
 
     @override_settings(CELERY_ALWAYS_EAGER=True, CELERY_EAGER_PROPAGATES_EXCEPTIONS=True, BROKER_BACKEND='memory')
-    @patch('entity_emailer.tasks.render_to_string')
     @patch('entity_emailer.tasks.get_email_addresses')
-    def test_from_field_pulled_from_custom_settings(self, address_mock, loader_mock):
+    def test_from_field_pulled_from_custom_settings(self, address_mock):
         """settings.DEFAULT_FROM_EMAIL should be passed through.
         """
-        loader_mock.side_effect = ['<p>This is a test html email.</p>',
-                                   'This is a test text email.']
+        template = G(EmailTemplate, text_template='Hi')
         address_mock.return_value = ['test1@example.com', 'test2@example.com']
         with patch('django.conf.settings.ENTITY_EMAILER_FROM_EMAIL', new='test_entity@example.com', create=True):
-            G(Email, context={}, scheduled=None)
+            G(Email, context={}, template=template, scheduled=None)
         from_email = mail.outbox[0].from_email
         self.assertEqual(from_email, 'test_entity@example.com')
 
