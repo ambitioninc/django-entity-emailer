6294c0eaf9b8eb10928110a70bdc4f0e4c375acc
Erik Swanson <theerikswanson@gmail.com>
2014-07-10 14:24:17 -0400

    Good looking email list-view.

--- entity_emailer/admin.py
+++ entity_emailer/admin.py
@@ -3,7 +3,13 @@ from entity_emailer.models import Email
 
 
 class EmailAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('subject', 'to', 'subentity_type', 'scheduled', 'has_been_sent')
 
+    def has_been_sent(self, obj):
+        return (obj.sent is not None)
+
+    def to(self, obj):
+        send_to_entity = obj.send_to
+        return send_to_entity.entity_meta.get('name', unicode(send_to_entity))
 
 admin.site.register(Email, EmailAdmin)
