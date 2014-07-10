7a26b1fa36a0ba5d49c3dc26dbed7bc46223145b
Erik Swanson <theerikswanson@gmail.com>
2014-07-10 16:37:19 -0400

    Only show relevant subentity types.

--- entity_emailer/admin.py
+++ entity_emailer/admin.py
@@ -1,16 +1,30 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
-from entity import Entity
+from entity import Entity, EntityRelationship
 
 from entity_emailer.models import Email
+
+
+def get_subentity_content_type_qs():
+    """Return a queryset of contenttypes of subentities.
+
+    This queryset filters the contenttypes table to only include the
+    contenttypes of subentities mirrored by the Entity framework.
+    """
+    return ContentType.objects.filter(
+        pk__in=EntityRelationship.objects.
+        select_related('sub_entity').
+        distinct('sub_entity__entity_type').
+        values('sub_entity__entity_type')
+    )
 
 
 class CreateEmailForm(forms.ModelForm):
     subject = forms.CharField(max_length=128)
     from_email = forms.EmailField()
     to_entity = forms.ModelChoiceField(queryset=Entity.objects.all())
-    subentity_type = forms.ModelChoiceField(queryset=ContentType.objects.all())
+    subentity_type = forms.ModelChoiceField(queryset=get_subentity_content_type_qs())
     body = forms.CharField(widget=forms.Textarea)
     scheduled = forms.DateTimeField()
 
