387dcda6fff3fac0d8506e047919ffe3c48591af
Erik Swanson <theerikswanson@gmail.com>
2014-07-17 16:10:41 -0400

    Bump django-entity to version 1.1.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'django>=1.6',
         'django-db-mutex>=0.1.3',
-        'django-entity>=1.0',
+        'django-entity>=1.1.2',
         'django-entity-subscription>=0.2.0',
         'celery>=3.1',
     ],
