18f5ee32a46af6eecc58eb86a73d5f812a16c817
Jared Lewis <jared.lewis@ambition.com>
2020-02-10 13:21:52 -0500

    Handle email exceptions

--- entity_emailer/interface.py
+++ entity_emailer/interface.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 
 from datetime import datetime
 
@@ -6,7 +7,7 @@ from django.core import mail
 from entity_event import context_loader
 
 from entity_emailer.models import Email
-from entity_emailer.signals import pre_send
+from entity_emailer.signals import pre_send, email_exception
 
 from entity_emailer.utils import get_medium, get_from_email_address, get_subscribed_email_addresses, \
     create_email_message, extract_email_subject_from_html_content
@@ -23,6 +24,7 @@ class EntityEmailerInterface(object):
         Send out any scheduled emails that are unsent
         """
 
+        # Get the emails that we need to send
         current_time = datetime.utcnow()
         email_medium = get_medium()
         to_send = Email.objects.filter(
@@ -32,15 +34,30 @@ class EntityEmailerInterface(object):
             'event__source'
         ).prefetch_related(
             'recipients'
+        ).order_by(
+            'scheduled',
+            'id'
         )
 
         # Fetch the contexts of every event so that they may be rendered
         context_loader.load_contexts_and_renderers([e.event for e in to_send], [email_medium])
 
+        # Keep track of what emails we will be sending
         emails = []
+
+        # Loop over each email and generate the recipients, and message
+        # and handle any exceptions that may occur
         for email in to_send:
+            # Compute what email addresses we actually want to send this email to
             to_email_addresses = get_subscribed_email_addresses(email)
-            if to_email_addresses:
+
+            # If there are no recipients we can just skip rendering
+            if not to_email_addresses:
+                continue
+
+            # If any exceptions occur we will catch the exception and store it as a reference
+            # As well as fire off a signal with the error and mark the email as sent and errored
+            try:
                 # Render the email
                 text_message, html_message = email.render(email_medium)
 
@@ -64,9 +81,23 @@ class EntityEmailerInterface(object):
 
                 # Add the email to the list of emails that need to be sent
                 emails.append(message)
+            except Exception as e:
+                # Save the exception on the model
+                email.exception = traceback.format_exc()
+                email.save(update_fields=['exception'])
 
+                # Fire the email exception event
+                email_exception.send(
+                    sender=Email,
+                    email=email,
+                    exception=e
+                )
+
+        # Send all the emails that were generated properly
         connection = mail.get_connection()
         connection.send_messages(emails)
+
+        # Update the emails as sent
         to_send.update(sent=current_time)
 
     @staticmethod
